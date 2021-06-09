318664a3b13305d6eb5c6528bb00cbde4aad47f3
aswin <aswinbenny999@gmail.com>
2021-06-09 13:12:16 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
-import 'music.dart';
+import 'happy.dart';
+import 'angry.dart';
+import 'neutral.dart';
+import 'sad.dart';
+import 'package:http/http.dart' as http;
+import 'package:convert/convert.dart';
+import 'dart:async';
+import 'package:async/async.dart';
+import 'package:path/path.dart';
+import 'package:http_parser/http_parser.dart';
+import 'dart:convert';
 void main()=> runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -27,6 +37,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  
 
   File _image;
 
@@ -35,6 +46,75 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _image = image;
     });
+
+    //Future<String> sendFileToAPI({File file})async{
+    var request = http.MultipartRequest(
+      'POST',
+      Uri.parse('http://192.168.1.8:5000/predict'),
+    );
+    Map<String, String>headers = {
+      "Content-type": "multipart/form-data",
+      "accept": "application/json"
+    };
+    request.files.add(
+      http.MultipartFile
+        ('file',
+        _image.readAsBytes().asStream(),
+        _image.lengthSync(),
+        filename: basename(_image.path),
+        contentType: MediaType('image', basename(_image.path)
+            .split(".")
+            .last),
+      ),
+    );
+    request.headers.addAll(headers);
+    try {
+      var res = await request.send();
+      var result= await res.stream.bytesToString();
+      final de= json.decode(result);
+      //int a=(result);
+      //print (a);
+      String a="0";
+      String b="1";
+      String c="2";
+      String d="3";
+      if(de==a)
+        {
+          print("hap");
+          Navigator.push(
+            this.context,
+            MaterialPageRoute(
+                builder: (context) => MyApph()
+            ),);
+        }
+      if(de==b)
+      { print("sad");
+        Navigator.push(
+          this.context,
+          MaterialPageRoute(
+              builder: (context) => MyApps()
+          ),);
+      }
+      if(de==c)
+      {print("neu");
+        Navigator.push(
+          this.context,
+          MaterialPageRoute(
+              builder: (context) => MyAppn()
+          ),);
+      }
+      if(de==d)
+      {print("ang");
+        Navigator.push(
+          this.context,
+          MaterialPageRoute(
+              builder: (context) => MyAppa()
+          ),);
+      }
+    } catch (err) {
+      print("ERROR: $err");
+      return "error";
+    }
   }
 
   @override
@@ -58,19 +138,6 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
 
-      floatingActionButton: IconButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => MyApps()
-            ),);
-        },
-
-        icon: Icon(Icons.add_circle),
-        color: Colors.pink,
-        iconSize: 70.0,
-      ),
     );
   }
 }
